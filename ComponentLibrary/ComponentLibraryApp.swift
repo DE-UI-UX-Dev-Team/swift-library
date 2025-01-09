cb1db760067649a7ff0043ce1a982aea81101499
nrg-susanruan <susan.ruan@nrg.com>
2025-01-09 14:49:00 -0500

    Initial Commit

---
+++ ComponentLibrary/ComponentLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentLibraryApp.swift
+//  ComponentLibrary
+//
+//  Created by susan ruan on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ComponentLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComponentLibraryTests/ComponentLibraryTests.swift
@@ -0,0 +1,16 @@
+//
+//  ComponentLibraryTests.swift
+//  ComponentLibraryTests
+//
+//  Created by susan ruan on 1/9/25.
+//
+
+import Testing
+
+struct ComponentLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
