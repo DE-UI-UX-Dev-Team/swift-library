a9d9643aaba9e23514398da3ec31108150443702
Christopher Coleman <chrismco@TXHQSGEORGE-L1.nrgenergy.com>
2025-02-11 08:59:40 -0600

    added gitignore file, header component, adjusted root app file, created searchable components upon main screen.

--- ComponentLibrary/ComponentLibraryApp.swift
+++ ComponentLibrary/ComponentLibraryApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ComponentLibraryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ComponentLibraryHome()
         }
     }
 }
